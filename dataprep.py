6c0098bf9b9e544acea62c95d8fe66c20e557f97
Hairuo Gao <40450609+HairuoGao029@users.noreply.github.com>
2022-10-07 21:14:09 +0800

    Add files via upload

---
+++ dataprep.py
@@ -0,0 +1,231 @@
+import numpy as np
+from xml.sax import parse, handler
+import os
+import random, shutil
+import pandas as pd
+import math
+
+class DetectorParse(handler.ContentHandler):
+    """reading in the dataset from xml file
+    """
+    def __init__(self):
+        self.lanes = {}
+        self.sample = {}
+
+    def startElement(self, name, attrs):
+        if name == 'timestep':
+            self.probe_id = attrs['id']
+
+
+        if name == 'vehicle':
+
+          pos = float(attrs['pos'])
+
+          pos_i = str(int(pos//1000))
+          lane_id = attrs['lane']+'%'+pos_i
+
+          if lane_id not in self.lanes:
+              self.lanes[lane_id] = {}
+              self.lanes[lane_id]['Speed'] = []
+              self.lanes[lane_id]['ID'] = []
+              self.lanes[lane_id]['AverageSpeed'] = []
+              self.sample[lane_id] = {}
+              self.sample[lane_id]['Original'] = []
+              self.sample[lane_id]['Attacker'] = []
+          self.lanes[lane_id]['Speed'].append(float(attrs['speed']))
+          self.lanes[lane_id]['ID'].append(str(attrs['id']))
+
+
+    def endElement(self, name):
+        if name == 'timestep':
+            if self.probe_id == 'probe2':
+              for key, value in self.lanes.items():
+                  n = len(value['Speed'])
+                  ave_speed = []
+
+                  if n > 0:
+                      rate = 0.3  # sample of benign users
+                      picknumber = math.ceil(n * rate)
+                      sample = random.sample(list(zip(value['Speed'], value['ID'])), picknumber)
+                      self.sample[key]['Original'].append(sample)
+                      speed, id = zip(*sample)
+                      std = np.std(speed)
+                      ave_speed = np.mean(speed)
+
+                  if n > 1:
+                      rate_att = 0.3  # sample of attackers
+                      picknumber_att = math.ceil(n * rate_att)
+                      remain_index = tuple(set(range(len(list(zip(value['Speed'], value['ID']))))) - set(sample))
+                      att_index = random.sample(remain_index, picknumber_att)
+                      sample_att = [list(zip(value['Speed'], value['ID']))[i] for i in att_index]
+                      self.sample[key]['Original'].append(sample_att)
+
+                      speed_att, id_att = zip(*sample_att)
+                      speed_att_new = np.random.normal(5, std, len(speed_att))
+                      ave_speed_tmp = []
+                      ave_speed_tmp.append(speed)
+                      ave_speed_tmp.append(speed_att_new)
+                      ave_speed = np.mean(ave_speed_tmp)
+                      self.sample[key]['Attacker'].append(list(zip(speed_att_new, id_att)))
+
+                  self.lanes[key]['AverageSpeed'].append(ave_speed)
+                  self.lanes[key]['Speed'] = []
+
+
+class DetectorParse_attacker(handler.ContentHandler):
+    """reading in the dataset from xml file
+    """
+
+    def __init__(self):
+        self.lanes = {}
+        self.sample = {}
+        self.lane_list = ['56250003#0_0%1', '237111029#1.254_0%0', '237111029#1.254_0%1', '237111029#1.254_1%0',
+                          '237111029#1.254_1%1', '238459506.6_0%1', '62830645#2.0.0_0%0', '62830645#2.0.0_0%1',
+                          '62830645#2.0.0_1%0', '62830645#2.0.0_1%1']
+
+    def startElement(self, name, attrs):
+        if name == 'timestep':
+            self.probe_id = attrs['id']
+            self.time = float(attrs['time'])
+
+        if name == 'vehicle':
+            pos = float(attrs['pos'])
+            pos_i = str(int(pos // 1000))
+            lane_id = attrs['lane'] + '%' + pos_i
+            if lane_id in self.lane_list and lane_id not in self.lanes:
+                self.lanes[lane_id] = {}
+                self.lanes[lane_id]['Speed'] = []
+                self.lanes[lane_id]['ID'] = []
+                # self.lanes[lane_id]['AverageSpeed'] = []
+                self.sample[lane_id] = {}
+                self.sample[lane_id]['Original'] = []
+                self.sample[lane_id]['Attacker'] = []
+            if (lane_id in ['56250003#0_0%1', '237111029#1.254_0%0', '237111029#1.254_0%1', '237111029#1.254_1%0','237111029#1.254_1%1'] and self.time >= 10800.00 and self.time <= 13200.00) or \
+                    (lane_id in ['238459506.6_0%1', '62830645#2.0.0_0%0', '62830645#2.0.0_0%1', '62830645#2.0.0_1%0','62830645#2.0.0_1%1'] and self.time >= 20400.00 and self.time <= 21600.00):
+                self.lanes[lane_id]['Speed'].append(float(attrs['speed']))
+                self.lanes[lane_id]['ID'].append(str(attrs['id']))
+
+    def endElement(self, name):
+        if name == 'timestep':
+            if self.probe_id == 'probe2':
+                for key, value in self.lanes.items():
+                    if (key in ['56250003#0_0%1', '237111029#1.254_0%0', '237111029#1.254_0%1', '237111029#1.254_1%0','237111029#1.254_1%1'] and self.time >= 10800.00 and self.time <= 13200.00) or \
+                            (key in ['238459506.6_0%1', '62830645#2.0.0_0%0', '62830645#2.0.0_0%1', '62830645#2.0.0_1%0','62830645#2.0.0_1%1'] and self.time >= 20400.00 and self.time <= 21600.00):
+                        # if (key in ['237111029#1.254_0%0','237111029#1.254_0%1'] and self.time >= 10800.00 and self.time <= 13200.00):
+
+                        # 15:00-15:40 #17:40-18:00
+                        n = len(value['Speed'])
+                        if n == 1:
+                            # rate = 0.3  # sample of benign users
+                            # picknumber = math.ceil(n * rate)
+                            # sample = random.sample(list(zip(value['Speed'], value['ID'])), picknumber)
+                            self.sample[key]['Original'].append(list(zip(value['Speed'], value['ID'])))
+                            self.sample[key]['Attacker'].append('None')
+
+                            # ave_speed = np.mean(speed)
+
+                        elif n > 1:
+                            rate = 0.3  # sample of benign users
+                            picknumber = math.ceil(n * rate)
+                            sample = random.sample(list(zip(value['Speed'], value['ID'])), picknumber)
+                            # self.sample[key]['Original'].append(sample)
+                            speed, id = zip(*sample)
+                            std = np.std(speed)
+
+                            rate_att = 0.3  # sample of attackers
+                            picknumber_att = math.ceil(n * rate_att)
+                            value['ID'] += 'att'
+                            remain_index = tuple(set(range(len(list(zip(value['Speed'], value['ID']))))) - set(sample))
+                            att_index = random.sample(remain_index, picknumber_att)
+                            sample_att = [list(zip(value['Speed'], value['ID']))[i] for i in att_index]
+                            # self.sample[key]['Original'].append(sample_att)
+
+                            speed_att, id_att = zip(*sample_att)
+                            speed_att_new = np.random.normal(5, 0.5, len(speed_att))
+                            '''
+                            ave_speed_tmp = []
+                            ave_speed_tmp.append(speed)
+                            ave_speed_tmp.append(speed_att_new)
+                            ave_speed = np.mean(ave_speed_tmp)
+                            '''
+                            sample += list(zip(speed_att_new, id_att))
+                            self.sample[key]['Original'].append(sample)
+                            self.sample[key]['Attacker'].append(id_att)
+
+                        # self.lanes[key]['AverageSpeed'].append(ave_speed)
+                        self.lanes[key]['Speed'] = []
+                        self.lanes[key]['ID'] = []
+
+    def endDocument(self):
+        # print(self.lanes)
+        print(self.sample)
+
+
+def connect2oneday(day, load_dict_1, load_dict_2, lane_list):
+
+    final = []
+    dicic = []
+    a = []
+    for i in range(len(lane_list)):
+      for key in load_dict_1:
+        #if key in load_dict_2:
+          if  key == lane_list[i]:
+            # print('key',key)
+            a = load_dict_1[key]['AverageSpeed'][-720:]
+            b = load_dict_2[key]['AverageSpeed'][:2160]
+            a.extend(b)
+
+            dicic.append(str(key))
+            dicic.append(a)
+
+            final.append(dicic)
+            dicic = []
+    print(len(final)) # (630, 1+720)
+
+    # replace Nan
+    df = final
+    a = []
+    n = []
+    df_new = df
+    for i in range(len(df)):
+        a = pd.DataFrame(df[i][1])
+        n = a.ffill()
+        n = n.bfill().values.tolist()
+        df_new[i][1] = list(np.array(n).ravel())
+        p = np.isnan(n)
+        if p is True:
+            print(i)
+        a = []
+        n = []
+    np.save('rawdata/npy/'+day+'_8h.npy', df_new)
+    return(df_new)
+
+
+def main():
+    input_path = 'rawdata/'
+    files = os.listdir(input_path)
+    files.sort()
+    # for file in files:
+    for file in ['d20_0-12.xml', 'd20_12-24.xml']:
+        if os.path.splitext(file)[1] == '.xml':
+            save_dir = 'rawdata/npy/' + os.path.splitext(file)[0]
+            detector_output = DetectorParse()
+            #detector_output = DetectorParse_attacker()
+            parse(input_path + file, detector_output)
+            np.save(save_dir+ '.npy', detector_output.lanes)
+            np.save(save_dir+ '_sample.npy', detector_output.lanes)
+
+    data = {}
+    lane_list = np.load('net/lane_list.npy')
+    for day in ['d20']:
+        load_dict_1 = np.load('rawdata/npy/'+day+'_0-12.npy', allow_pickle=True).item()
+        load_dict_2 = np.load('rawdata/npy/'+day+'_12-24.npy', allow_pickle=True).item()
+        d_day = connect2oneday(day, load_dict_1, load_dict_2, lane_list)
+        data[day] = d_day
+
+    final = data['d20']
+    np.save('rawdata/npy/8h_d20_fill.npy', final)
+
+
+if __name__ == "__main__":
+    main()
---
+++ model/dcrnn_model.py
@@ -0,0 +1,162 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+from lib.metrics import masked_mae_loss
+from model.dcrnn_cell import DCGRUCell
+
+
+def rnn_decoder(decoder_inputs,
+                initial_state,
+                cell,
+                loop_function=None,
+                scope=None):
+    """RNN decoder for the sequence-to-sequence model.
+    Args:
+      decoder_inputs: A list of 2D Tensors [batch_size x input_size].
+      initial_state: 2D Tensor with shape [batch_size x cell.state_size].
+      cell: rnn_cell.RNNCell defining the cell function and size.
+      loop_function: If not None, this function will be applied to the i-th output
+        in order to generate the i+1-st input, and decoder_inputs will be ignored,
+        except for the first element ("GO" symbol). This can be used for decoding,
+        but also for training to emulate http://arxiv.org/abs/1506.03099.
+        Signature -- loop_function(prev, i) = next
+          * prev is a 2D Tensor of shape [batch_size x output_size],
+          * i is an integer, the step number (when advanced control is needed),
+          * next is a 2D Tensor of shape [batch_size x input_size].
+      scope: VariableScope for the created subgraph; defaults to "rnn_decoder".
+    Returns:
+      A tuple of the form (outputs, state), where:
+        outputs: A list of the same length as decoder_inputs of 2D Tensors with
+          shape [batch_size x output_size] containing generated outputs.
+        state: The state of each cell at the final time-step.
+          It is a 2D Tensor of shape [batch_size x cell.state_size].
+          (Note that in some cases, like basic RNN cell or GRU cell, outputs and
+           states can be the same. They are different for LSTM cells though.)
+    """
+    with tf.variable_scope(scope or "rnn_decoder"):
+        state = initial_state
+        outputs = []
+        prev = None
+        for i, inp in enumerate(decoder_inputs):
+            if loop_function is not None and prev is not None:
+                with tf.variable_scope("loop_function", reuse=True):
+                    inp = loop_function(prev, i)
+            if i > 0:
+                tf.get_variable_scope().reuse_variables()
+            output, state = cell(inp, state)
+            outputs.append(output)
+            if loop_function is not None:
+                prev = output
+    return outputs, state
+
+
+class DCRNNModel(object):
+    def __init__(self, is_training, batch_size, scaler, adj_mx, **model_kwargs):
+        # Scaler for data normalization.
+        self._scaler = scaler
+
+        # Train and loss
+        self._loss = None
+        self._mae = None
+        self._train_op = None
+
+        max_diffusion_step = int(model_kwargs.get('max_diffusion_step', 2))
+        cl_decay_steps = int(model_kwargs.get('cl_decay_steps', 1000))
+        filter_type = model_kwargs.get('filter_type', 'laplacian')
+        horizon = int(model_kwargs.get('horizon', 1))
+        max_grad_norm = float(model_kwargs.get('max_grad_norm', 5.0))
+        num_nodes = int(model_kwargs.get('num_nodes', 1))
+        num_rnn_layers = int(model_kwargs.get('num_rnn_layers', 1))
+        num_units = int(model_kwargs.get('rnn_units'))
+        seq_len = int(model_kwargs.get('seq_len'))
+        use_curriculum_learning = bool(model_kwargs.get('use_curriculum_learning', False))
+        input_dim = int(model_kwargs.get('input_dim', 1))
+        output_dim = int(model_kwargs.get('output_dim', 1))
+
+        # Input (batch_size, timesteps, num_sensor, input_dim)
+        self._inputs = tf.placeholder(tf.float32, shape=(batch_size, seq_len, num_nodes, input_dim), name='inputs')
+        # Labels: (batch_size, timesteps, num_sensor, input_dim), same format with input except the temporal dimension.
+        self._labels = tf.placeholder(tf.float32, shape=(batch_size, horizon, num_nodes, input_dim), name='labels')
+
+        # GO_SYMBOL = tf.zeros(shape=(batch_size, num_nodes * input_dim))
+        GO_SYMBOL = tf.zeros(shape=(batch_size, num_nodes * output_dim))
+
+        cell = DCGRUCell(num_units, adj_mx, max_diffusion_step=max_diffusion_step, num_nodes=num_nodes,
+                         filter_type=filter_type)
+        cell_with_projection = DCGRUCell(num_units, adj_mx, max_diffusion_step=max_diffusion_step, num_nodes=num_nodes,
+                                         num_proj=output_dim, filter_type=filter_type)
+        encoding_cells = [cell] * num_rnn_layers
+        decoding_cells = [cell] * (num_rnn_layers - 1) + [cell_with_projection]
+        encoding_cells = tf.nn.rnn_cell.MultiRNNCell(encoding_cells, state_is_tuple=True)
+        decoding_cells = tf.nn.rnn_cell.MultiRNNCell(decoding_cells, state_is_tuple=True)
+
+        global_step = tf.train.get_or_create_global_step()
+        # Outputs: (batch_size, timesteps, num_nodes, output_dim)
+        with tf.variable_scope('DCRNN_SEQ'):
+            inputs = tf.unstack(tf.reshape(self._inputs, (batch_size, seq_len, num_nodes * input_dim)), axis=1)
+            labels = tf.unstack(
+                tf.reshape(self._labels[..., :output_dim], (batch_size, horizon, num_nodes * output_dim)), axis=1)
+            labels.insert(0, GO_SYMBOL)
+
+            def _loop_function(prev, i):
+                if is_training:
+                    # Return either the model's prediction or the previous ground truth in training.
+                    if use_curriculum_learning:
+                        c = tf.random_uniform((), minval=0, maxval=1.)
+                        threshold = self._compute_sampling_threshold(global_step, cl_decay_steps)
+                        result = tf.cond(tf.less(c, threshold), lambda: labels[i], lambda: prev)
+                    else:
+                        result = labels[i]
+                else:
+                    # Return the prediction of the model in testing.
+                    result = prev
+                return result
+
+            _, enc_state = tf.nn.static_rnn(encoding_cells, inputs, dtype=tf.float32)
+            outputs, final_state = rnn_decoder(labels, enc_state, decoding_cells,
+                                                              loop_function=_loop_function)
+
+
+        # Project the output to output_dim.
+        outputs = tf.stack(outputs[:-1], axis=1)
+        self._outputs = tf.reshape(outputs, (batch_size, horizon, num_nodes, output_dim), name='outputs')
+        self._merged = tf.summary.merge_all()
+
+    @staticmethod
+    def _compute_sampling_threshold(global_step, k):
+        """
+        Computes the sampling probability for scheduled sampling using inverse sigmoid.
+        :param global_step:
+        :param k:
+        :return:
+        """
+        return tf.cast(k / (k + tf.exp(global_step / k)), tf.float32)
+
+    @property
+    def inputs(self):
+        return self._inputs
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def loss(self):
+        return self._loss
+
+    @property
+    def mae(self):
+        return self._mae
+
+    @property
+    def merged(self):
+        return self._merged
+
+    @property
+    def outputs(self):
+        return self._outputs
